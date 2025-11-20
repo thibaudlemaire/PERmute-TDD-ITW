a4f82832bc0bde5372c3280334293bf964ea24e6
Thibaud Lemaire <thibaudl@theodo.fr>
2025-11-20 16:26:11 +0000

    Entretien PP

--- src/Transfer.php
+++ src/Transfer.php
@@ -5,14 +5,44 @@ class Transfer
     /** @var array<TransferEvent> */
     private $events = [];
     private TransferDirection $direction;
+    private TransferState $state;
 
     public function __construct($events, $direction)
     {
         $this->events = $events;
         $this->direction = $direction;
+        $this->state = TransferState::DRAFT;
     }
 
     // TODO : implement nextReminder calculation
+    public function nextReminder()
+    {
+        if ($this->direction == TransferDirection::OUTGOING) {
+            return null;
+        }
+
+        if ($this->state == TransferState::CLOSED) {
+            return null;
+        }
+
+        if (in_array($this->state, TransferState::inCompanyPending) ) {
+            return null;
+        }
+
+        
+
+        return new Datetime();
+    }
+
+    public function addEvent(TransferEvent $ev)
+    {
+        $events[] = $ev;
+    }
+
+    public function setState(TransferState $state)
+    {
+        $this->state = $state;
+    }
     
 }
 
@@ -30,4 +60,7 @@ enum TransferState: string
     case DEMAND_ACCEPTED = 'DEMAND_ACCEPTED';
     case VOUCHER_REVIEW = 'VOUCHER_REVIEW';
     case CLOSED = 'CLOSED';
+
+    public const inCompanyPending = [self::DRAFT, self::DEMAND_REJECTED, self::VOUCHER_REVIEW];
+    public const outCompanyPending = [self::DEMAND_REVIEW, self::DEMAND_ACCEPTED];
 }
--- tests/TransferTest.php
+++ tests/TransferTest.php
@@ -8,4 +8,42 @@ class TransferTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testNoReminderIfOutgoing()
+    {
+        $transfer = new Transfer([], TransferDirection::OUTGOING);
+        $this->assertNull($transfer->nextReminder());
+    }
+
+    public function testNoReminderIfTransferIsClosed()
+    {
+        $transfer = new Transfer([], TransferDirection::INCOMING);
+        $transfer->setState(TransferState::CLOSED);
+        $this->assertNull($transfer->nextReminder());
+    }
+
+    public function testNoReminderIfInCompanyIsPending()
+    {
+        $transfer = new Transfer([], TransferDirection::INCOMING);
+
+        $transfer->setState(TransferState::DRAFT);
+        $this->assertNull($transfer->nextReminder());
+        
+        $transfer->setState(TransferState::DEMAND_REJECTED);
+        $this->assertNull($transfer->nextReminder());
+
+        $transfer->setState(TransferState::VOUCHER_REVIEW);
+        $this->assertNull($transfer->nextReminder());
+    }
+
+    public function testReminderAfter30Days()
+    {
+        $transfer = new Transfer([], TransferDirection::INCOMING);
+
+        $transferDate = new DateTime("01/01/2024 10:00:00");
+        $transferEvent = new TransferEvent($transferDate->format('Y-m-d H:i:s'), TransferEventType::EVT_PER_DEMAND_SENT);
+        $transfer->addEvent($transferEvent);
+
+        $this->assertTrue($transfer->nextReminder()->format('Y-m-d') == $transferDate->modify('+30 days')->format('Y-m-d'));
+    }
 }
